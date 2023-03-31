9911f18c8159ec87205bd114a3be7427663d51c2
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-03-31 17:31:03 +0100

    refactoring and code cleaning

--- index.mjs
+++ index.mjs
@@ -12,14 +12,14 @@ dotenv.config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -28,17 +28,18 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 //const auth = getAuth(app);
 
-    const sha256 = (input) => {
-        const hash = crypto.createHash('sha256');
-        hash.update(input);
-        return hash.digest('hex');
-    }
+const sha256 = (input) => {
+    const hash = crypto.createHash('sha256');
+    hash.update(input);
+    return hash.digest('hex');
+}
 
 const createUserId = () => {
-   const nanoidId = nanoid();
-   return sha256(nanoidId);   
-}  
-const setReference = (pathreference) => ref(db, pathreference);
+    const nanoidId = nanoid();
+    return sha256(nanoidId);
+}
+
+const setReference = (pathreference = '' || undefined) => ref(db, pathreference);
 
 const hashPasswordWithBcrypt = (passwordPlaintext) => {
     const saltRounds = 10;
@@ -48,13 +49,17 @@ const hashPasswordWithBcrypt = (passwordPlaintext) => {
                 resolve(hash)
             } else {
                 reject(err)
-            }    
-        });    
-    })  
+            }
+        });
+    })
 }
 
-const emailExists = (email) => {
-
+const verifyUserPassword = (password, passwordHash) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, passwordHash, (err, result) => {
+            resolve(result);
+        });
+    })
 }
 /*
 const registerUserWithEmailAndPassword = (email, password) => {
@@ -72,106 +77,104 @@ const registerUserWithEmailAndPassword = (email, password) => {
 */
 
 const checkUserExists = (email) => {
-    const emailHash = sha256(email);
+    const emailHash = sha256(email.toLowerCase());
+    const dbref = setReference();
+    return new Promise((resolve, reject) => {
 
-    get(ref(db, 'testfoo'))
-    .then((snapshot)=>{
-        console.log(snapshot.val());
+        get(child(dbref, "mynewlist/" + emailHash))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const user = snapshot.val();
+                    console.log('user exists');
+                    resolve(user)
+                } else {
+                    console.log('user does not exist');
+                    resolve(false)
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     })
-    .catch((error)=>{
-        console.log(error);
-    });
-
-
-
 }
 
-async function registerUserWithEmailAndPassword(email, password){
-    //TODO check if a user already exists before proceeding
-    const userId = createUserId();
-    const ref = setReference('mynewlist/'+sha256(email.toLowerCase()));
-    const user = {
-        uid:userId,
-        email,
-        password:await hashPasswordWithBcrypt(password)
-    }
-    set(ref,user)
-    .then(()=>{
-        console.log(`succesfully added!`)
-    })
-    .catch((error)=>{
-        console.log(error);
-    });
-    }
+async function registerUserWithEmailAndPassword(email, password) {
+    const userExist = await checkUserExists(email);
 
-function FindData() {
-           // const dbref = ref(db);
-
-            get(ref(db, 'testfoo'))
-                .then((snapshot)=>{
-                    console.log(snapshot.val());
-                })
-                .catch((error)=>{
-                    console.log(error);
-                });
-            }
-
-//FindData();
-
-function UpdateData(){
-            update(ref(db, "testObj"),{
-                alpha:"dddd"
+    if (!userExist) {
+        const userId = createUserId();
+        const ref = setReference('mynewlist/' + sha256(email.toLowerCase()));
+        const user = {
+            uid: userId,
+            email,
+            password: await hashPasswordWithBcrypt(password)
+        }
+        set(ref, user)
+            .then(() => {
+                console.log(`succesfully added!`)
             })
-            .then(()=>{
-                console.log("Data updated successfully");
-            })
-            .catch((error)=>{
+            .catch((error) => {
                 console.log(error);
             });
-        }
 
-        //UpdateData();
-
-        //const objref = ref(db,'testObj/alpha');
-        //const child = child(objref,'users'),
-        //set(objref,"boum3iza").then(()=>{console.log('success');})
-        /*
-        get(objref).then(( snapshot)=>{
-
-        const old = snapshot.val();
-        old.push('c');
-        old.push('d');
-        console.log('old',old);
-
-        set(objref,old).then(()=>{console.log('updated');});
-        //console.log('new',newVal);
-
-        }).catch((error)=>{console.log(error)})
-*/
-        
-//registerUserWithEmailAndPassword("test1234@gmail.com","12345678");
-    
-
-
-//const myPlaintextPassword = 's0/\/\P4$$w0rD';
-//const someOtherPlaintextPassword = 'not_bacon';
-
-/*
-bcrypt.compare(someOtherPlaintextPassword, "$2b$10$/61KxLwfVdMB7uQILEdbweYJR1YdubTmGjGrcWoII6vHKJwb5fLBK", (err, result) => {
-    // result == false
-    console.log(result);
-});
-*/
-
-async function test() {
-    const hash = await hashPasswordWithBcrypt('momento');
-    console.log(hash);
+    } else {
+        return 'user already exists'
+    }
 }
 
-/*
-hashPasswordWithBcrypt('salut').then((hash)=>{
-    console.log(hash);
-})
-*/
+const getUser = (email) => {
+    const dbref = setReference();
+    const emailHash = sha256(email.toLowerCase());
 
-registerUserWithEmailAndPassword('bob@test.com','bobpassword','bob');
+    return new Promise((resolve, reject) => {
+        get(child(dbref, "mynewlist/" + emailHash))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const user = snapshot.val();
+                    resolve(user)
+                } else {
+                    resolve(false)
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    })
+
+}
+
+const loginUser = async (email, password) => {
+    const user = await getUser(email);
+    const passwordVerified = await verifyUserPassword(password, user.password);
+    if (user && passwordVerified) {
+        return 'user logged in';
+    } else {
+        return 'unable to login please check your credentials';
+    }
+
+}
+
+function FindData() {
+    // const dbref = ref(db);
+
+    get(ref(db, 'testfoo'))
+        .then((snapshot) => {
+            console.log(snapshot.val());
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function UpdateData() {
+    update(ref(db, "testObj"), {
+        alpha: "dddd"
+    })
+        .then(() => {
+            console.log("Data updated successfully");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
