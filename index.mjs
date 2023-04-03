c92e6638ea8d96b775887175d30880dc2de4b08e
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-04-03 15:55:11 +0100

    adding features and error handling

--- index.mjs
+++ index.mjs
@@ -314,6 +314,21 @@ server.post('/addnewdocumentusertodb', async (req, res) => {
 
 })
 
+// Error handling middleware
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+  // Process event listeners
+  process.on('uncaughtException', function(err) {
+    console.error('Uncaught Exception:', err.stack);
+  });
+  
+  process.on('unhandledRejection', function(reason, promise) {
+    console.error('Unhandled Rejection:', reason.stack || reason);
+  });
+
 server.listen(port, () => {
     console.log(`docdigitalizordb service listening at http://localhost:${port}`);
 });
